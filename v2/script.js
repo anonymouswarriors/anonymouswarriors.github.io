fb67d61d736f2f07b28132313abd6ed0d9fef043
Mr. Anonymous <113576776+anonymouswarriors@users.noreply.github.com>
2022-10-09 01:30:15 +0530

    Add files via upload

---
+++ v2/script.js
@@ -0,0 +1,78 @@
+window.onload = () => {
+    const myInput = document.getElementById('dummyKeyboard');
+    myInput.onpaste = e => e.preventDefault();
+}
+const COMMANDS = {
+    whoami: 'root@𝕻𝖍𝖆𝖓𝖙𝖔𝖒',
+    8055: 'Supported commands: <span class="code">tools</span>, <span class="code">files</span>, <span class="code">showme</span>',
+    tools: 'Supported commands: <span class="code">hackbar</span>, <span class="code">shell</span>, <span class="code">ransomeware</span><span class="code">rat</span>',
+    ls: 'Supported commands: <span class="code">notes</span>, <span class="code">hacking video</span>, <span class="code">pdf course</span><span class="code">links</span>',
+    showme: '<img src="./img/showme.png">',
+
+    hackbar: 'Supported commands: <span class="code">download</span>, <span class="code">install</span>',
+    download: "<a href='./HBM Hackbar 2020-v.07.1.rar' class='success link'><img src='./img/hkbar.png'></a> <a href='./Alien 2020 Red Button Theme.rar' class='success link'><img src='./img/theme.png'></a>",
+    install: "<video width='350' height='220' controls><source src='./HBM.mp4' type='video/mp4'></a>",
+
+
+    help: '<embed src="./scarylaugh.mp3"autostart style="visibility:hidden" type="audio/mpeg"></embed>',
+    fuck: '<img src="./img/fkst.webp" alt="WebP rules." width="320" height="240"><embed src="./fk.mp3"autostart style="visibility:hidden" type="audio/mpeg"></embed>',
+
+
+    help: 'Supported commands: <span class="code">about</span>, <span class="code">experience</span>, <span class="code">education</span>, <span class="code">skills</span>, <span class="code">acknowledges</span>, <span class="code">certificates</span>, <span class="code">contact</span>',
+    about: "Hello 👋<br>I'm 𝕿𝖍𝖊 𝕻𝖍𝖆𝖓𝖙𝖔𝖒 ✘. Information Security enthusiast, I do bug bounty hunting, Capture The Flag as a hobby and passion, Grapic Design, 2D Animation,...",
+    skills: '<span class="code">Skill:</span>Pentesting, Programming, Bughunting, Grapic Design, 2D Animation, ...<br>',
+    education: "unknown",
+    certificates: "<a href='https://www.alienshanu.me/certificates' class='success link'>certificates</a>",
+    resume: "<a href='./resume.pdf' class='success link'>resume.pdf</a>",
+    experience: "No Experience😥",
+    acknowledges: "<a href='xxxx' class='success link'>Google Hall of Fame</a> , <a href='xxxx' class='success link'>Nokia HOF</a> , <a href='xxxx' class='success link'>dev.to HOF</a> , <a href='xxxx' class='success link'>Netflix HOF</a> , <a href='xxxx' class='success link'>Pinterest HOF</a> , <a href='xxxx' class='success link'>Philips HOF</a> , <a href='xxxx' class='success link'>Soundcloud HOF</a> , <a class='success link'>many more...</a>  ",
+    contact: "You can contact me on any of following :<br><a href='https://t.me/Alien_Shanu/' class='success link'><i class='fab fa-telegram icon'></i>Telegram</a>,<a href='https://www.instagram.com/alien_shanu/' class='success link'><i class='fab fa-instagram icon'></i>Instagram</a>,  <a href='https://www.twitter.com/Alien_Shanu/' class='success link'><i class='fab fa-twitter icon'></i>Twitter</a>, <a href='mailto:mail@blackalien@pm.me' class='success link'><i class='far fa-envelope-open icon'></i>Email</a>, <a href='https://api.whatsapp.com/send?phone=+966547818575' class='success link'><i class='fab fa-whatsapp icon'></i>whatsapp</a>",
+
+};
+
+const userInput = document.getElementById("userInput");
+const terminalOutput = document.getElementById("terminalOutput");
+const inputfield = document.getElementById("dummyKeyboard");
+
+inputfield.addEventListener('keypress', (e) => {
+    if (e.key === "Enter") {
+        let input = e.target.value;
+        input = input.toLowerCase();
+        if (input.length === 0) {
+            return;
+        }
+        let output;
+        output = `<div class="terminal-line"><span class="code1">>_</span> <span class="success">$root</span><span class="directory">@𝕻𝖍𝖆𝖓𝖙𝖔𝖒:~$</span> ${input}</div>`;
+        if (!COMMANDS.hasOwnProperty(input)) {
+            output += `<div class="terminal-line">no such command: ${input}</div>`;
+            console.log("Oops! no such command");
+        } else {
+            output += COMMANDS[input];
+        };
+        terminalOutput.innerHTML = `${
+      terminalOutput.innerHTML
+    }<div class="terminal-line">${output}</div>`;
+        terminalOutput.scrollTop = terminalOutput.scrollHeight;
+        e.target.value = ""
+
+    };
+
+
+
+});
+
+function show() {
+    document.getElementById('share-btn-container').classList.toggle('active');
+}
+function showTerminal() {
+    document.getElementById('hero').classList.toggle('active');
+}
+function showTerminal(){
+    if(hero==1){
+        document.getElementById("hero").style.display="inline"
+        return hero=0;
+    }else{
+       document.getElementById("hero").style.display="none" 
+        return hero=1;
+    }
+}
